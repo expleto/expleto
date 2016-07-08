85dda437793475351d94467130b782f73c5c06da
Valeriy Solovyov <weldpua2008@gmail.com>
2016-07-08 02:09:46 -0400

    adding read data from file

--- utils/utils.go
+++ utils/utils.go
@@ -4,7 +4,7 @@ import (
 	// "bytes"
 	// "encoding/xml"
 	"fmt"
-	// "io/ioutil"
+	"io/ioutil"
 	"path/filepath"
 	"runtime"
 	// "strconv"
@@ -17,4 +17,15 @@ import (
 func FormatError(err error) error {
 	pc, fn, line, _ := runtime.Caller(1)
 	return fmt.Errorf("%v\nTRACE: %s[%s:%d]", err, runtime.FuncForPC(pc).Name(), filepath.Base(fn), line)
+}
+
+func GetDataFromFile(path string) ([]byte, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
 }
